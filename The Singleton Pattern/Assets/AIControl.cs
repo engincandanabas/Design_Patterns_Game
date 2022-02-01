4e3cdd7d81133f6f79f53d3ee8ed6b7fcc15b9c8
Engincan Danabaş <60680749+engincandanabas@users.noreply.github.com>
2022-02-01 22:00:11 +0300

    The Singleton Pattern

---
+++ The Singleton Pattern/Assets/AIControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+	GameObject[] goalLocations;
+	UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+    Vector3 lastGoal;
+
+
+	// Use this for initialization
+	void Start () {
+		goalLocations = GameObject.FindGameObjectsWithTag("goal");
+		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("isWalking", true);
+        PickGoalLocation();
+	}
+
+    void PickGoalLocation()
+    {
+        lastGoal = agent.destination;
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (agent.remainingDistance < 1) //At the goal
+        {
+            PickGoalLocation();
+        }
+	}
+}
---
+++ The Singleton Pattern/Assets/DropGarbage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropGarbage : MonoBehaviour {
+
+    public GameObject garbage;
+
+    void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Instantiate(garbage,hitInfo.point,Quaternion.identity);
+            }
+        }
+    }
+}
